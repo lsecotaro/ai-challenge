77135c799d1642da7a94767b2ebd71a92b7f91cd
leonardosecotaro <lsecotaro@nocnocstore.com>
2025-01-07 18:22:41 -0300

    Upload from csv file and process by batches

--- src/modules/interfaces/ai.actions.ts
+++ src/modules/interfaces/ai.actions.ts
@@ -1,4 +1,3 @@
-import { OpenAIMessageDto } from '../integrations/open-ia/dto/OpenAIMessageDto';
 
 export interface AiActions {
   cleanData(input: string): Promise<string>;
--- src/modules/vehicle-service/vehicle-service.controller.ts
+++ src/modules/vehicle-service/vehicle-service.controller.ts
@@ -1,16 +1,36 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { VehicleService } from './vehicle.service';
 import { VehicleServiceRequestDto } from './dto/vehicle-service-request.dto';
 import { EnrichedVehicleServiceDto } from './dto/enriched/enriched-vehicle-service.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CsvParserService } from '../commons/csv-parser.service';
 
 @Controller('vehicle-service')
 export class VehicleServiceController {
-  constructor(private readonly vehicleService: VehicleService) {}
+  constructor(
+    private readonly vehicleService: VehicleService,
+    private readonly csvParserService: CsvParserService,
+  ) {}
 
   @Post('clean')
   async cleanNews(
     @Body() request: Array<VehicleServiceRequestDto>,
   ): Promise<Array<EnrichedVehicleServiceDto> | void> {
     return await this.vehicleService.processNews(request);
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('csv'))
+  async uploadCsv(@UploadedFile() file: Express.Multer.File) {
+    const buffer = file.buffer;
+    const parsedData = await this.csvParserService.parseCsv(buffer);
+    return this.vehicleService.processUploadCsv(parsedData, file.originalname);
   }
 }
